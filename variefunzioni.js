eaf261d32ccbbd559c10343b19ffd1c2fc316cb6
FrancescoMorettiBrs <morettifrancesco.lav@gmail.com>
2025-10-08 11:26:07 +0200

    feat: snack 2 completed

--- variefunzioni.js
+++ variefunzioni.js
@@ -1,6 +1,16 @@
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione getInitials restituisce le iniziali di un nome completo."
+
 function getInitials(fullname) {
   const parts = fullname.trim().split(/\s+/);
   return (parts[0][0] + parts[parts.length - 1][0]).toUpperCase();
 }
 
-module.exports = { getInitials };
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione createSlug restituisce una stringa in lowercase."
+
+function createSlug(str) {
+  return str.toLowerCase();
+}
+
+module.exports = { getInitials, createSlug };
--- varitest.test.js
+++ varitest.test.js
@@ -1,6 +1,11 @@
-const { getInitials } = require("./variefunzioni");
+const { getInitials, createSlug } = require("./variefunzioni");
 
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
   const result = getInitials("Francesco Moretti");
   expect(result).toBe("FM");
+});
+
+test("La funzione createSlug restituisce una stringa in lowercase", () => {
+  const result = createSlug("SONO UNA STRINGA IN MINUSCOLO");
+  expect(result).toBe("sono una stringa in minuscolo");
 });
